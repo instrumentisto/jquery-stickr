9b2fdcc2c949d0b9b9bbee122d1cfd2f01623a7c
tyranron <tyranron@gmail.com>
2015-11-14 00:07:21 +0200

    1) reimplemented with new API 2) removed any styles manipulations from JS

--- README.md
+++ README.md
@@ -5,5 +5,46 @@
 [![Bower license](https://img.shields.io/bower/l/jquery-stickr.svg)](http://docs.jquery.com/License)
 [![Dependency Status](https://gemnasium.com/instrumentisto/jquery-stickr.svg)](https://gemnasium.com/instrumentisto/jquery-stickr)
 
-
 jQuery plugin that allows to create lightweight stickers on HTML page.
+You may use them for error messages, system notifications and
+a lot of other UI stuff.
+
+
+## Installation
+Include script *after* the jQuery library: 
+```html
+<script src="/path/to/jquery.js"></script>
+<script src="/path/to/jquery.stickr.js"></script>
+<link rel="stylesheet" href="/path/to/jquery.stickr.css">
+```
+
+
+## Usage
+Plugin provides next API:
+```javascript
+$sticker = $.stickr({
+  afterOpen: function($sticker) {
+    console.log($sticker, 'is rendered');
+  },
+  beforeClose: function($sticker) {
+    console.log($sticker, 'will be closed in 500 ms');
+  },
+  classes: 'some additional classes here',
+  closingDuration: 500,   // default is 0
+  group: 'my-stickrs',    // or $('#my-stickrs'), default is 'jq-stickrs'
+  message: '<i>text<br>to be displayed</i>',
+  ttl: 5000,              // default is 0 which means "forever"
+  withCloseButton: false  // do not draw "Close" button, default is true
+});
+```
+Checkout `demo.html` for more interactive examples of usage.
+
+
+## Stylization
+No direct styles manipulation are applied from JS code. Instead of that, JS code
+of plugin manipulates only with classes of elements, so visual behaviour of
+elements can be fully reimplemented via CSS styles as desired.  
+You may implement your own total stylization giving desired styles for
+`.jq-stickrs`, `.jq-stickr`, `.jq-stickr--closable` and `jq-stickr__close`
+classes, just check `jquery.stickr.css` to see how they fit to each other.
+Or you may use already defined styles in `jquery.stickr.css` as starting point.
--- jquery.stickr.js
+++ jquery.stickr.js
@@ -1,62 +1,81 @@
 /*!
- * jQuery.stickr plugin v0.0.1
+ * jQuery.stickr plugin v0.1.0
  * https://github.com/instrumentisto/jquery-stickr
  *
  * Copyright 2015 Instrumentisto Team (https://github.com/instrumentisto)
  * Released under the MIT license
  * http://jquery.org/license
  */
+
 $.stickr = function(options) {
   var o = $.extend({
+        afterOpen: null,
+        beforeClose: null,
         classes: '',
-        fadeSpeed: 'slow',  // or in milliseconds (ms)
-        maxCount: 5,        // maximum number of concurrently displayed stickers
-        onClose: null,
-        onShow: null,
-        sticked: false,     // omit closing button and close by timeout
-        text: '',
-        ttl: 5000           // in milliseconds (ms)
+        closingDuration: 0,  // in milliseconds (ms)
+        group: 'jq-stickrs',
+        maxCount: 5,  // maximum number of concurrently displayed stickers
+        message: '',
+        ttl: 0,       // in milliseconds (ms)
+        withCloseButton: true
       }, options),
 
-      id = 'jq-stickrs',
-      $stickers = $('#' + id),
+      $stickers = o.group instanceof jQuery ? o.group : $('#' + o.group),
       $sticker = $('<div class="jq-stickr"></div>'),
-      endFunction = function() {
-        var $sticker = $(this);
-        if (typeof o.onClose == 'function') {
-          o.onClose($sticker);
+
+      isClosing = false,
+      close = function($sticker) {
+        if ($sticker.data('isClosing')) {
+          return;
         }
-        $sticker.remove();
+        $sticker.data('isClosing', true);
+        if (typeof o.beforeClose === 'function') {
+          o.beforeClose($sticker);
+        }
+        setTimeout(function() {
+          $sticker.remove();
+        }, o.closingDuration);
       };
 
-  if (!$stickers.length) {
-    $stickers = $('<div class="jq-stickrs" id="' + id + '"></div>')
+  if (!$stickers.length && !(o.group instanceof jQuery)) {
+    $stickers = $('<div class="jq-stickrs" id="' + o.group + '"></div>')
         .appendTo('body');
   }
 
-  $sticker.html(o.text);
+  $sticker.html(o.message);
   if (o.classes) {
     $sticker.addClass(o.classes)
   }
-  if (o.sticked) {
-    $sticker.addClass('jq-stickr--sticked');
-    $('<button class="jq-stickr__exit"></button>')
-        .prependTo($sticker)
-        .click(function() {
-          $sticker.fadeOut(o.fadeSpeed, endFunction);
-        });
-  } else {
+  if (o.withCloseButton) {
+    $sticker
+        .addClass('jq-stickr--closable')
+        .prepend(
+            $('<button class="jq-stickr__close"></button>')
+                .click(function() {
+                  close($sticker);
+                }));
+  }
+  if (o.ttl > 0) {
     setTimeout(function() {
-      $sticker.fadeOut(o.fadeSpeed, endFunction);
+      close($sticker);
     }, o.ttl);
   }
+
   $stickers.append($sticker);
-  if (typeof o.onShow == 'function') {
-    o.onShow($sticker);
+  if ($stickers.children().length > o.maxCount) {
+    close($stickers.children().first());
   }
 
-  if ($stickers.children().length > o.maxCount) {
-    $stickers.children().first().slideUp(endFunction);
+  if (typeof o.afterOpen === 'function') {
+    // Dirty hack to be sure that browser has rendered $sticker element.
+    // It is 5ms, because setTimeout(function() {}, 0) produces races and
+    // sometimes timeout fires before $sticker element is rendered.
+    // Also, window.requestAnimationFrame() solution is not used
+    // due to bad browsers support at the moment.
+    setTimeout(function() {
+      o.afterOpen($sticker);
+    }, 5);
   }
-  return $stickers;
+
+  return $sticker;
 };
